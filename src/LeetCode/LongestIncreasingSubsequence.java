001eca7985245b45fabfdefa14aa376c1d7b14c0
ArkadiusN <arek.nowacki4@gmail.com>
2024-01-05 22:59:30 +0000

    Chapter 1 Fundamentals Alternative package 1

---
+++ src/LeetCode/LongestIncreasingSubsequence.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+public class LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLIS(new int[]{0,1,0,3,2,3}));
+        System.out.println(lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
+        System.out.println(lengthOfLIS(new int[]{7,7,7,7,7}));
+        System.out.println(lengthOfLIS(new int[]{1,2,7,7,7}));
+        System.out.println(lengthOfLIS(new int[]{0}));
+    }
+
+
+    public static int lengthOfLIS(int[] nums) {
+        int lowest = Integer.MAX_VALUE;
+        int count = 0;
+        int back = 0;
+        int back2 = 0;
+        if(nums.length == 1){
+            return 1;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            back = nums[nums.length-1];
+            back2 = nums[nums.length-2];
+            if(lowest > nums[i-1]){
+                lowest = nums[i-1];
+            }
+            if(lowest < nums[i]){
+                count++;
+            }
+            if(nums[i-1] == nums[i]){
+                return 1;
+            }
+        }
+        if(back < back2){
+            count = count - 1;
+        }
+        return count;
+    }
+}
---
+++ src/LeetCode/RichestCustomerWealth.java
@@ -0,0 +1,53 @@
+package LeetCode;
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        System.out.println(maximumWealth(new int[][]
+                {{1,2,3},
+                {1,2,4}}
+        ));
+        System.out.println(maximumWealth(new int[][]
+                {{1,5},
+                {7,3},
+                {3,5}}
+        ));
+        System.out.println(maximumWealth(new int[][]
+                {{2,8,7},
+                {7,1,3},
+                {1,9,5}}
+        ));
+    }
+
+    /**
+     * You are given an m * x integer grid 'accounts' where 'accounts[i][j]'
+     * <br>
+     * is the amount of money the 'ith' customer has in the 'jth' bank.
+     * <br>
+     * Return the wealth that the richest customer has. A customer's wealth
+     * <br>
+     * is the amount of money they have in all their bank accounts. The richest
+     * <br>
+     * customer is the customer that has the maximum wealth.
+     * <br>
+     * <br>
+     * Complexity of <b>O(N * M)</b>
+     *
+     *
+     * @param accounts the integer grid with the amount of money each customer has in 'jth' bank.
+     * @return the sum of bank accounts that is the largest.
+     */
+    public static int maximumWealth(int[][] accounts){
+        int richest = Integer.MIN_VALUE;
+        int temp = 0;
+        for (int i = 0; i < accounts.length ; i++) {
+            for (int j = 0; j < accounts[i].length; j++) {
+                temp += accounts[i][j];
+            }
+            if (temp > richest){
+                richest = temp;
+            }
+            temp = 0;
+        }
+        return richest;
+    }
+}
