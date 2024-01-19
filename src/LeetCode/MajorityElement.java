99853231241aaba2c3c4723a16a9a2971bf5ce49
ArkadiusN <arek.nowacki4@gmail.com>
2024-01-19 22:53:20 +0000

    Chapter 1 Fundamentals Alternative package 12

--- src/LeetCode/MajorityElement.java
+++ src/LeetCode/MajorityElement.java
@@ -1,18 +1,33 @@
 package LeetCode;
 
+import edu.princeton.cs.algs4.StdOut;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MajorityElement {
     public static void main(String[] args) {
-        System.out.println(majorityElement(new int[]{3,2,3}));
-        System.out.println(majorityElement(new int[]{2,2,1,1,1,2,2}));
+        StdOut.println(majorityElement(new int[]{3,2,3}));
+        StdOut.println(majorityElement(new int[]{2,2,1,1,1,2,2}));
     }
 
-
+    /**
+     * Given an array nums of size n, return the majority element.
+     * <br>
+     * The majority element is the element that appears more than
+     * <br>
+     * '⌊n / 2⌋' times. You may assume that the majority element
+     * <br>
+     * always exists in the array
+     * <br>
+     * <br>
+     * Complexity of <b>O(N)</b>
+     *
+     * @param nums array of integers.
+     * @return element that appears more than half of the array size.
+     */
     public static int majorityElement(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
+        int major = 0;
         for (int j : nums) {
             if (map.containsKey(j)) {
                 map.put(j, map.get(j) + 1);
@@ -20,23 +35,11 @@ public class MajorityElement {
                 map.put(j, 1);
             }
         }
-        int max = 0;
-        for (Integer c: map.values()) {
-            if(c > max){
-                max = c;
+        for (Integer i : map.keySet()) {
+            if (map.containsValue(map.get(i)) && map.get(i) > nums.length/2) {
+                major = i;
             }
         }
-
-        int h = 0;
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
-            if(entry.getValue() == max) {
-                h = entry.getKey();
-                break;
-            }
-        }
-        if(max > n/2){
-            return h;
-        }
-        return -1;
+        return major;
     }
 }
---
+++ src/LeetCode/NumberOf1Bits.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class NumberOf1Bits {
+    public static void main(String[] args) {
+        StdOut.println(hammingWeight1(521));     //00000000000000000000000000001011
+        StdOut.println(hammingWeight1(2097152)); //00000000000000000000000010000000
+        StdOut.println(hammingWeight1(-3));      //11111111111111111111111111111101
+
+        StdOut.println(hammingWeight2(521));     //00000000000000000000000000001011
+        StdOut.println(hammingWeight2(2097152)); //00000000000000000000000010000000
+        StdOut.println(hammingWeight2(-3));      //11111111111111111111111111111101
+    }
+
+    /**
+     * Complexity of <b>O(1)</b>
+     *
+     * @param n binary representation of signed integer type.
+     * @return number of '1' bits.
+     */
+    public static int hammingWeight1(int n) {
+        return Integer.bitCount(n);
+    }
+
+    /**
+     * Write a function that takes the binary representation of
+     * <br>
+     * an unsigned integer and returns the number of '1' bits
+     * <br>
+     * it has (also known as the Hamming weight).
+     * <br>
+     * <br>
+     * Complexity of <b>O(LogN) + O(M)</b>
+     *
+     * @param n binary representation of signed integer type.
+     * @return number of '1' bits.
+     */
+    public static int hammingWeight2(int n) {
+        String str = Integer.toBinaryString(n); //LogN
+        str = str.replaceAll("0", ""); //M
+        return str.length();
+    }
+}
