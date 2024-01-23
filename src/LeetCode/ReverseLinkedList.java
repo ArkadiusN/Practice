13471daad822c8cd97e2c0455fd2a88d52460c0b
ArkadiusN <arek.nowacki4@gmail.com>
2024-01-23 22:50:07 +0000

    Chapter 1 Fundamentals Alternative package 16

---
+++ src/LeetCode/ReverseLinkedList.java
@@ -0,0 +1,41 @@
+package LeetCode;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        reverseList(new ListNode(5,
+                new ListNode(4,
+                        new ListNode(3,
+                new ListNode(2,
+                        new ListNode(1))))));
+    }
+    public static class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+        while (current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+//        while (head != null){
+//            StdOut.print(head.val + " ");
+//            head = head.next;
+//        }
+        return head;
+    }
+}
