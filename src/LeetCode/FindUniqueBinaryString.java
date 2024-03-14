8f7002c5dbb1001ca5dafa90c18253175d45bd10
ArkadiusN <arek.nowacki4@gmail.com>
2024-03-14 19:29:49 +0000

    Chapter 1 Fundamentals Alternative package 67

--- src/LeetCode/FindUniqueBinaryString.java
+++ src/LeetCode/FindUniqueBinaryString.java
@@ -2,13 +2,28 @@ package LeetCode;
 
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.Arrays;
-
 public class FindUniqueBinaryString {
     public static void main(String[] args) {
         StdOut.println(findDifferentBinaryString(new String[]{"01", "10"}));
     }
 
+    /**
+     * Given an array of strings nums containing n
+     * <br>
+     * unique binary strings each of length n, return
+     * <br>
+     * a binary string of length n that does not appear
+     * <br>
+     * in nums. If there are multiple answers, you may return any of them.
+     * <br>
+     * Complexity of <b>O(N)</b>
+     * <br>
+     * Space complexity <b>O(N)</b>
+     *
+     *
+     * @param nums array of binary strings
+     * @return binary string that does not appear in nums.
+     */
     public static String findDifferentBinaryString(String[] nums) {
         char[] str = new char[nums.length];
         for(int i = 0; i < nums.length; i++) {
