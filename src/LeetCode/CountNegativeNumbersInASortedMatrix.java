577b43c026df67d5619c8743ec69c2391ff4f621
ArkadiusN <arek.nowacki4@gmail.com>
2024-03-21 18:55:46 +0000

    Chapter 1 Fundamentals Alternative package 74

--- src/LeetCode/CountNegativeNumbersInASortedMatrix.java
+++ src/LeetCode/CountNegativeNumbersInASortedMatrix.java
@@ -7,6 +7,22 @@ public class CountNegativeNumbersInASortedMatrix {
         StdOut.println(countNegatives(new int[][]{{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}}));
     }
 
+    /**
+     * Given a m x n matrix grid which is sorted
+     * <br>
+     * in non-increasing order both row-wise and
+     * <br>
+     * column-wise, return the number of negative numbers in grid.
+     * <br>
+     * <br>
+     * Complexity of <b>O(N*M)</b>
+     * <br>
+     * Space complexity <b>O(1)</b>
+     *
+     *
+     * @param grid integer matrix to check
+     * @return total amount of negative values in matrix.
+     */
     public static int countNegatives(int[][] grid) {
         int counter = 0;
         for (int i = 0; i < grid.length; i++) {
